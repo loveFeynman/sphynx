2fb295644c71cb2ae7a5b4d61230bf5571c89f1e
loveFeynman <wando0226@gmail.com>
2021-08-24 16:00:21 +0800

    Toggle Menu (v1, v2)

--- src/views/Swap/components/ToggleList.tsx
+++ src/views/Swap/components/ToggleList.tsx
@@ -1,30 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 
-import { Text, Button, Link } from '@pancakeswap/uikit'
 // eslint-disable-next-line import/no-unresolved
 import './dropdown.css'
-import axios from 'axios';
-import {Button as materialButton,Menu,MenuItem} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import { typeRouterVersion } from '../../../state/input/actions'
 
-const MenuWrapper = styled.div`
-  padding: 0 4px;
+const ToggleWrapper = styled.div`
+  position: relative;
+  z-index: 2;
   height: 40px;
-  background: rgba(0, 0, 0, 0.4);
   width: 420px;
   border-radius: 16px;
   margin: 12px 0;
+  padding: 0 4px;
+  background: rgba(0, 0, 0, 0.4);
   display: flex;
   align-items: center;
+  ${({ theme }) => theme.mediaQueries.sm} {
+    min-width: 420px;
+  }
   & button {
     background: transparent !important;
     outline: none;
     box-shadow: none !important;
     padding: 0 12px;
     border: none;
+  }
+`
+
+const MenuWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  background: #131313;
+  color: #eee;
+  top: 45px;
+  overflow-y: auto;
+  max-height: 100vh;
+  ${({ theme }) => theme.mediaQueries.md} {
+    max-height: 600px;
   }
 `
 
@@ -38,52 +57,61 @@ const MenuTitle = styled.div`
   padding: 0px 12px;
 `
 
+const ContractPanelOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  left: 0;
+  top: 0;
+`
+
 export default function ToggleList() {
 
   const dispatch = useDispatch()
 
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [showDrop, setShowDrop] = useState(false)
-  const [selectedItem, setSelectedItem] = useState('')
-  const data = ["V1", "V2"]
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [selectedItem, setSelectedItem] = React.useState('V1')
+  
+  const [showDrop,setShowDrop] = useState(false)
 
-  const handleClick = (event:any) => {
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
     setShowDrop(true)
   };
+
   const handleClose = () => {
     setAnchorEl(null)
-  }
+    setShowDrop(false)
+  };
     
   return (
-    <>
-      <MenuWrapper>
+    <ToggleWrapper>
+      <Button aria-controls="fade-menu1" aria-haspopup="true" onClick={handleClick}>
         <MenuTitle>
           <span>{selectedItem}</span>
         </MenuTitle>
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-          <ArrowDropDownIcon/>
-        </Button>
-        {showDrop ? <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >{data.length > 0 ?
-        <span>
-          {data?.map((item:any )=>{
-            return <MenuItem key={item} onClick={()=> {
-              setSelectedItem(item)
-              setAnchorEl(null)
-              dispatch(typeRouterVersion({ routerVersion: item }))
-            }}>{item}</MenuItem>
-          })}
-        </span> : 
-          <span style={{ padding: '0 16px' }}>no record</span>}
-        </Menu>:""}
-      </MenuWrapper>
-    </>
+        <ArrowDropDownIcon style={{color: '#fff'}}/>
+      </Button>
+      {
+        showDrop &&
+        <MenuWrapper>
+          <MenuItem onClick={() => {
+            setSelectedItem('V1')
+            dispatch(typeRouterVersion({ routerVersion: 'V1' }))
+            handleClose()
+          }}>V1</MenuItem>
+          <MenuItem onClick={() => {
+            setSelectedItem('V2')
+            dispatch(typeRouterVersion({ routerVersion: 'V2' }))
+            handleClose()
+          }}>V2</MenuItem>
+        </MenuWrapper>
+      }
+      { showDrop && <ContractPanelOverlay onClick={() => setShowDrop(false) } />}
+    </ToggleWrapper>
   )
 }
 
