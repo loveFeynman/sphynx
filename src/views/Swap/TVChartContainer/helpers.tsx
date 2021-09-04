ae822ed597557c9bb9c711c67063a458095583b1
loveFeynman <wando0226@gmail.com>
2021-09-04 12:04:33 +0200

    refactor: format code

--- src/views/Swap/TVChartContainer/helpers.tsx
+++ src/views/Swap/TVChartContainer/helpers.tsx
@@ -1,44 +1,47 @@
 // Make requests to CryptoCompare API
 
-
-export async function makeApiRequest(path:any) {
-    try {
-        const response = await fetch(`https://api.sphynxswap.finance/tokenDetails/${path}`);
-        return response.json();
-    } catch (error) {
-        throw new Error(`CryptoCompare request error: ${error.status}`);
-    }
+export async function makeApiRequest(path: any) {
+  try {
+    const response = await fetch(`https://api.sphynxswap.finance/tokenDetails/${path}`)
+    return response.json()
+  } catch (error) {
+    throw new Error(`CryptoCompare request error: ${error.status}`)
+  }
 }
-export async function makeApiRequest1(path:any, routerVersion: any, resolution: any) {
-    try {
-        // console.log("here in api reuest",path)
-        const response = await fetch(`https://api.sphynxswap.finance/${routerVersion === 'v1' ? 'v1/' : ''}chart/${path}?resolution=${resolution}`, {
-            method : "GET",
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              }});
-        return response.json();
-    } catch (error) {
-        throw new Error(`CryptoCompare request error: ${error.status}`);
-    }
+export async function makeApiRequest1(path: any, routerVersion: any, resolution: any) {
+  try {
+    // console.log("here in api reuest",path)
+    const response = await fetch(
+      `https://api.sphynxswap.finance/${routerVersion === 'v1' ? 'v1/' : ''}chart/${path}?resolution=${resolution}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    return response.json()
+  } catch (error) {
+    throw new Error(`CryptoCompare request error: ${error.status}`)
+  }
 }
 // Generate a symbol ID from a pair of the coins
-export function generateSymbol(exchange:any, fromSymbol:any, toSymbol:any) {
-    const short = `${fromSymbol}/${toSymbol}`;
-    return {
-        short,
-        full: `${exchange}:${short}`,
-    };
+export function generateSymbol(exchange: any, fromSymbol: any, toSymbol: any) {
+  const short = `${fromSymbol}/${toSymbol}`
+  return {
+    short,
+    full: `${exchange}:${short}`,
+  }
 }
-export function parseFullSymbol(fullSymbol:any) {
-    const match = fullSymbol.match(/^(\w+):(\w+)\/(\w+)$/);
-    if (!match) {
-        return null;
-    }
-    return {
-        exchange: match[1],
-        fromSymbol: match[2],
-        toSymbol: match[3],
-    };
+export function parseFullSymbol(fullSymbol: any) {
+  const match = fullSymbol.match(/^(\w+):(\w+)\/(\w+)$/)
+  if (!match) {
+    return null
+  }
+  return {
+    exchange: match[1],
+    fromSymbol: match[2],
+    toSymbol: match[3],
+  }
 }
