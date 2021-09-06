b0b818291a5f0274ed61616ecf728b745e97767d
loveFeynman <wando0226@gmail.com>
2021-09-06 22:56:25 +0800

    update dividend panel

--- src/views/Swap/components/DividendPanel.tsx
+++ src/views/Swap/components/DividendPanel.tsx
@@ -2,20 +2,29 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Flex, Text, Button, useModal } from '@pancakeswap/uikit'
 import DividendModal from 'components/Menu/GlobalSettings/DividendModal'
-import QuestionHelper from 'components/QuestionHelper'
 import MainLogo from 'assets/svg/icon/logo_new.svg'
+import MoreIcon from 'assets/svg/icon/MoreIcon2.svg'
 
 const Wrapper = styled.div`
   background: black;
   border-radius: 16px;
-  padding: 12px 20px;
+  padding: 8px 20px;
   color: white;
+  position: relative;
   & img {
     width: 48px;
   }
   & button {
     outline: none;
   }
+`
+
+const DetailsImage = styled.img`
+  position: absolute;
+  right: 0;
+  width: auto !important;
+  height: 40px;
+  cursor: pointer;
 `
 
 const DividendPanel:React.FC = () => {
@@ -26,14 +35,12 @@ const DividendPanel:React.FC = () => {
       <Flex>
         <img src={MainLogo} alt='Main Logo' />
         <Text color='white' mt={2} ml={1}>Sphynx Dividend</Text>
+        <DetailsImage src={MoreIcon} alt='More Icon' onClick={() => onPresentDividendModal()} />
       </Flex>
       <Flex justifyContent='space-between' mt={2}>
         <Text color='white' fontSize='14px'>Amount to be Distributed</Text>
         <Text color='white' fontSize='14px'>$ 12,000</Text>
       </Flex>
-      <Button width='100%' height='36px' mt={2} onClick={() => onPresentDividendModal()}>
-        Show Details
-      </Button>
     </Wrapper>
   )
 }
