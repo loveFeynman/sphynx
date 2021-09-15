7737a5a65bded293bcd378f13a28b6c8c7f98e1c
loveFeynman <wando0226@gmail.com>
2021-09-15 23:29:09 +0800

    update transaction card

--- src/views/Swap/components/TransactionCard.tsx
+++ src/views/Swap/components/TransactionCard.tsx
@@ -3,13 +3,15 @@
 /* eslint-disable no-console */
 import { Flex } from '@pancakeswap/uikit'
 import axios from 'axios'
-import { providers, utils } from 'ethers'
+import { providers, utils, Contract } from 'ethers'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+import bscTokenAbi from 'config/abi/erc20.json'
 import { useDerivedSwapInfo } from 'state/swap/hooks'
 import { Field } from 'state/swap/actions'
-import styled from 'styled-components'
-import { getTokenPrice } from 'state/info/ws/priceData'
+import { getTokenPrice, getPancakePairAddress, getMinTokenInfo } from 'state/info/ws/priceData'
+import { WBNB } from 'config/constants/tokens'
 import { AppState } from '../../../state'
 import { isAddress } from '../../../utils'
 
@@ -62,10 +64,10 @@ const TableWrapper = styled.div`
 
 const TransactionCard = () => {
   const input = useSelector<AppState, AppState['inputReducer']>((state) => state.inputReducer.input)
-  const isInput = useSelector<AppState, AppState['inputReducer']>((state) => state.inputReducer.isInput)
   const result = isAddress(input)
-  const [socketData, setSocketData]: any = useState({})
   const [dataArr, setDataArr] = useState([])
+  const zeroAddress = '0x0000000000000000000000000000000000000000';
+  const isInput = useSelector<AppState, AppState['inputReducer']>((state) => state.inputReducer.isInput)
   const { currencies } = useDerivedSwapInfo()
   const token = currencies[isInput ? Field.OUTPUT : Field.INPUT]
 
@@ -124,49 +126,125 @@ const TransactionCard = () => {
     }
   }`
 
-  const pcsRouterV2Addr = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
-
   useEffect(() => {
-    if (token) {
-      const tokenData = token.symbol.toLowerCase()
-      const provider = new providers.WebSocketProvider('wss://bsc-ws-node.nariox.org:443');
-  
-      const array = []
-
-      provider.on('pending', async(tx) => {
-        const transaction = await provider.getTransaction(tx);
-        if (transaction) {
-          const txAmount = Number(utils.formatUnits(transaction.value));
-          if (txAmount > 0 && transaction.to.includes(pcsRouterV2Addr)) {
-            const detailsHash = await provider.waitForTransaction(transaction.hash);
-            if(detailsHash.logs && detailsHash.status === 1) {
-              const block = await provider.getBlock(detailsHash.blockNumber);
-              const priceToList = await getTokenPrice('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', provider);
-              const dataObj = {
-                timestamp: block.timestamp,
-                amount: txAmount,
-                price: priceToList,
-                hash: transaction.hash
-              }
-              if (array.length < 30) {
-                array.unshift(dataObj);
-              } else {
-                array.pop();
-                array.unshift(dataObj);
-              }
-              setDataArr(array);
-            }
-          }  
-        }
-      })
-
+    if (!result) {
+      return;
     }
+    const provider = new providers.WebSocketProvider('wss://bsc-ws-node.nariox.org:443');
+    const tokenContract = new Contract(result, bscTokenAbi, provider);
+    const array = [];
+    setDataArr([]);
+    let blockNumber = 0;
+    let transactions = [];
+    let cakeBnbLpAddress = '';
+    let tokenInfo;
+    let txHashs = [];
+
+    (async() => {
+      cakeBnbLpAddress = await getPancakePairAddress(result, WBNB.address, provider);
+      tokenInfo = await getMinTokenInfo(result, provider);
+    })();
+
+    const checkTrans = async (blockNo) => {
+      const block = await provider.getBlock(blockNo);
+      let tokenPrice = 0;
+      try {
+        tokenPrice = await getTokenPrice(result, provider)
+      } catch (e) {
+        tokenPrice = 0;
+      }
+      // const cakePrice = 0;
+      return new Promise((resolve) => {
+        for (let i = 0; i < transactions.length; i++) {
+          const tokenAmount = utils.formatUnits(transactions[i].amount, tokenInfo.decimals)
+          // if buy transaction, from must be lp pair
+          let isBuy;
+          if (transactions[i].from === cakeBnbLpAddress) {
+            isBuy = true;
+          }
+          // if sell transaction, to must be lp pair
+          if (transactions[i].to === cakeBnbLpAddress) {
+            isBuy = false;
+          }
+
+          const transObj = {
+            timestamp: block.timestamp * 1000,
+            amount: tokenAmount,
+            price: tokenPrice,
+            isBuy
+          }
+
+          if (array.length < 30) {
+            array.unshift(transObj);
+          } else {
+            array.pop();
+            array.unshift(transObj);
+          }
+          console.log('bbb', array);
+          setDataArr(array);
+        }
+        txHashs = []
+        transactions = []
+        resolve(true);
+      })
+    }
+
+    tokenContract.on("Transfer", async (from, to, amount, event) => {
+      if (from === zeroAddress || to === zeroAddress) {
+        return;
+      }
+      if (blockNumber < event.blockNumber && blockNumber !== 0) {
+        const prevBlock = blockNumber;
+        blockNumber = event.blockNumber;
+        await checkTrans(prevBlock);
+        return;
+      }
+      blockNumber = event.blockNumber;
+      if (txHashs.indexOf(event.transactionHash)) {
+        txHashs.push(event.transactionHash);
+        transactions.push({
+          from,
+          to,
+          amount,
+          blockNumber: event.blockNumber,
+          transactionHash: event.transactionHash
+        });
+      }
+    });
+
+
+    // provider.on('pending', async(tx) => {
+    //   const transaction = await provider.getTransaction(tx);
+    //   if (transaction) {
+    //     const txAmount = Number(utils.formatUnits(transaction.value));
+    //     if (txAmount > 0 && transaction.to.includes(pcsRouterV2Addr)) {
+    //       const detailsHash = await provider.waitForTransaction(transaction.hash);
+    //       if(detailsHash.logs && detailsHash.status === 1) {
+    //         const block = await provider.getBlock(detailsHash.blockNumber);
+    //         const priceToList = await getTokenPrice('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', provider);
+    //         const dataObj = {
+    //           timestamp: block.timestamp,
+    //           amount: txAmount,
+    //           price: priceToList,
+    //           hash: transaction.hash
+    //         }
+    //         if (array.length < 30) {
+    //           array.unshift(dataObj);
+    //         } else {
+    //           array.pop();
+    //           array.unshift(dataObj);
+    //         }
+    //         setDataArr(array);
+    //       }
+    //     }  
+    //   }
+    // })
 
     // effect cleanup function
-    return () => {
-      setDataArr([])
-    }
-  }, [token]) // <-- empty dependency array
+    // return () => {
+    //   setDataArr([]);
+    // }
+  }, [result]) // <-- empty dependency array
 
   // useEffect(() => {
   //   dataArr.map((dt) => {
@@ -203,26 +281,25 @@ const TransactionCard = () => {
           </thead>
           <tbody>
             {dataArr.map((data) => {
-              const t = data.timestamp * 1000
+              const t = data.timestamp
               const localdate = new Date(t)
               const d = new Date(localdate.getTime() + localdate.getTimezoneOffset() * 60 * 1000)
               const offset = localdate.getTimezoneOffset() / 60
               const hours = localdate.getHours()
               const lcl = d.setHours(hours - offset)
               const date = new Date(lcl)
-              const isRecent = (new Date()).getTime() - t > 10000
               return (
                 <tr>
                   <td style={{ width: '35%' }}>
                     <Flex alignItems="center">
-                      <h2 className={isRecent ? 'success' : 'error'}>{date.toString().split('GMT')[0]}</h2>
+                      <h2 className={data.isBuy ? 'success' : 'error'}>{date.toString().split('GMT')[0]}</h2>
                     </Flex>
                   </td>
                   <td style={{ width: '25%' }}>
-                    <h2 className={isRecent ? 'success' : 'error'}>{ token.name }</h2>
+                    <h2 className={data.isBuy ? 'success' : 'error'}>{ token.name }</h2>
                   </td>
                   <td style={{ width: '25%' }}>
-                    <h2 className={isRecent ? 'success' : 'error'}>
+                    <h2 className={data.isBuy ? 'success' : 'error'}>
                       $
                       {Number(data.price)
                         .toFixed(4)
