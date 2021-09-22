acb2381f835338a9c35b104d19880ce94b466b78
loveFeynman <wando0226@gmail.com>
2021-09-22 09:17:33 -0400

    fix bitQuery api key

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -5,7 +5,8 @@ export const SNAPSHOT_VOTING_API = process.env.REACT_APP_SNAPSHOT_VOTING_API
 export const SNAPSHOT_BASE_URL = process.env.REACT_APP_SNAPSHOT_BASE_URL
 export const SNAPSHOT_API = `${SNAPSHOT_BASE_URL}/graphql`
 export const SNAPSHOT_HUB_API = `${SNAPSHOT_BASE_URL}/api/message`
-export const BITQUERY_API = 'https://graphql.bitquery.io/BQYtw5cN2icONV51CELWQAq387ZAOAtr/'
+export const BITQUERY_API = 'https://graphql.bitquery.io/'
+export const BITQUERY_API_KEY = 'BQYtw5cN2icONV51CELWQAq387ZAOAtr'
 
 export const WS_BSC_NODE = 'wss://old-thrumming-voice.bsc.quiknode.pro/7674ba364cc71989fb1398e1e53db54e4fe0e9e0/'
 
