6ae7d6cbad85d2ecdb7ff389404b40764674b89b
loveFeynman <wando0226@gmail.com>
2021-09-28 03:31:20 +0100

    feat: return node URL based on the chainId

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -2,9 +2,11 @@ import sample from 'lodash/sample'
 
 // Array of available nodes to connect to
 export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
+const allNodes = JSON.parse(process.env.REACT_APP_NODES)
 
 const getNodeUrl = () => {
-  return sample(nodes)
+  const chainId = process.env.REACT_APP_CHAIN_ID
+  return sample(allNodes[chainId])
 }
 
 export default getNodeUrl
