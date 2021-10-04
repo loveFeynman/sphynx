24207d1c12e7cce1974d0a27ac037206f264857d
loveFeynman <wando0226@gmail.com>
2021-10-04 01:19:33 -0500

    fetch token candle data on the frontend.

--- src/views/Swap/components/Chart/PcsChartContainer/helpers.tsx
+++ src/views/Swap/components/Chart/PcsChartContainer/helpers.tsx
@@ -1,5 +1,8 @@
 // Make requests to CryptoCompare API
 
+import axios from 'axios'
+import { getVersion } from '../../../../../utils/getVersion'
+
 export async function makeApiRequest(path: any) {
   try {
     const response = await fetch(`https://thesphynx.co/api/tokenDetails/${path}`)
@@ -10,16 +13,34 @@ export async function makeApiRequest(path: any) {
 }
 export async function makeApiRequest1(path: any, routerVersion: any, resolution: any) {
   try {
-    const response = await fetch(
-      `https://thesphynx.co/api/${routerVersion === 'v1' ? 'v1/' : ''}chart/${path}?resolution=${resolution}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    if (!path) {
+      return null;
+    }
+    const till = new Date().toISOString();
+    const resolutionMap = {
+      1: "1m",
+      5: "5m",
+      10: "10m",
+      15: "15m",
+      30: "30m",
+      60: "1h",
+      "1D": "1d",
+      "1W": "1w",
+      "1M": "1M",
+    };
+    const version = await getVersion(path);
+    const url = `https://api2.poocoin.app/candles-bsc?to=${till}&limit=320&lpAddress=${version.pairAddress}&interval=${resolutionMap[resolution]}&baseLp=0x1B96B92314C44b159149f7E0303511fB2Fc4774f`;
+    const { data } = await axios.get(url, {
+      headers: {
+        origin: "https://poocoin.app",
+        referer: "https://poocoin.app/",
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "user-agent": "PostmanRuntime/7.26.8",
+        Connection: "keep-alive",
       },
-    )
-    return response.json()
+    });
+    return data;
   } catch (error) {
     throw new Error(`CryptoCompare request error: ${error.status}`)
   }
