a16d88f7d2661a89451b746cc062e4a2647d66a2
loveFeynman <wando0226@gmail.com>
2021-10-08 03:40:18 -0400

    optimize real-time data

--- src/views/Swap/index.tsx
+++ src/views/Swap/index.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable no-console */
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+/* eslint-disable */
+import axios from 'axios'
+import React, { useCallback, useEffect, useMemo, useState, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppState } from 'state'
 import { autoSwap } from 'state/flags/actions'
@@ -66,6 +67,35 @@ import SwapWarningModal from './components/SwapWarningModal'
 import DividendPanel from './components/DividendPanel'
 import { Field, replaceSwapState } from '../../state/swap/actions'
 
+import Web3 from 'web3'
+import ERC20ABI from 'assets/abis/erc20.json'
+import routerABI from 'assets/abis/pancakeRouter.json'
+import { getPancakePairAddress, getPancakePairAddressV1 } from 'state/info/ws/priceData'
+import * as ethers from 'ethers'
+import { simpleRpcProvider } from 'utils/providers'
+import { priceInput, amountInput } from 'state/input/actions'
+import { UNSET_PRICE, DEFAULT_VOLUME_RATE } from 'config/constants/info'
+const pancakeV2: any = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+const pancakeV1: any = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+const metamaskSwap: any = '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31'
+const busdAddr = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
+const wBNBAddr = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
+const sphynxAddr = '0x2e121Ed64EEEB58788dDb204627cCB7C7c59884c'
+let tokenDecimal = 18
+
+const abi: any = ERC20ABI
+const routerAbi: any = routerABI
+
+let config = {
+  headers: {
+    'X-API-KEY': BITQUERY_API_KEY,
+  },
+}
+
+const providerURL = 'https://speedy-nodes-nyc.moralis.io/fbb4b2b82993bf507eaaab13/bsc/mainnet/archive'
+const web3 = new Web3(new Web3.providers.HttpProvider(providerURL))
+const pancakeRouterContract = new web3.eth.Contract(routerAbi, pancakeV2)
+
 const ArrowContainer = styled(ArrowWrapper)`
   width: 32px;
   height: 32px;
@@ -116,16 +146,281 @@ const SwapPage = styled(Page)`
   padding: 0;
 `
 
+const getBNBPrice: any = async () => {
+  return new Promise(async (resolve) => {
+    let path = [wBNBAddr, busdAddr]
+    pancakeRouterContract.methods
+      .getAmountsOut(web3.utils.toBN(1 * Math.pow(10, tokenDecimal)), path)
+      .call()
+      .then((data) => resolve(parseFloat(ethers.utils.formatUnits(data[data.length - 1] + '', 18))))
+  })
+}
+
 export default function Swap({ history }: RouteComponentProps) {
   const dispatch = useDispatch()
   const { pathname } = useLocation()
   const tokenAddress = pathname.substr(6)
   const [swapRouter, setSwapRouter] = useState(SwapRouter.AUTO_SWAP)
+  const [pairs, setPairs] = useState([])
+  const [transactionData, setTransactions] = useState([])
+  const stateRef = useRef([])
+  const pairsRef = useRef([])
+  const [isLoading, setLoading] = useState(false)
+  const [isBusy, setBusy] = useState(false)
+  const [volumeRate, setVolumeRate] = useState(DEFAULT_VOLUME_RATE)
   const swapFlag = useSelector<AppState, AppState['autoSwapReducer']>((state) => state.autoSwapReducer.swapFlag)
 
+  stateRef.current = transactionData
+  pairsRef.current = pairs
+  let input = tokenAddress
+  if (input === '-' || input === '') input = sphynxAddr
+  const contract: any = new web3.eth.Contract(abi, input)
+
+  const getDataQuery = useCallback(() => {
+    return `
+    {
+    ethereum(network: bsc) {
+        dexTrades(
+        options: {desc: ["block.height", "tradeIndex"], limit: 30, offset: 0}
+        date: {till: null}
+        baseCurrency: {is: "${input}"}
+        quoteCurrency:{is : "${wBNBAddr}"}
+        ) {
+        block {
+          timestamp {
+          time(format: "%Y-%m-%d %H:%M:%S")
+          }
+          height
+        }
+        tradeIndex
+        protocol
+        exchange {
+          fullName
+        }
+        smartContract {
+          address {
+          address
+          annotation
+          }
+        }
+        baseAmount
+        baseCurrency {
+          address
+          symbol
+        }
+        quoteAmount
+        quoteCurrency {
+          address
+          symbol
+        }
+        transaction {
+          hash
+        }
+        buyCurrency {
+          symbol
+          address
+          name
+        }
+        sellCurrency {
+          symbol
+          address
+          name
+          }
+        price
+        quotePrice
+        }
+      }
+    }`
+  }, [input])
+
   React.useEffect(() => {
+    const ab = new AbortController()
+    const getPairs = async () => {
+      let pairs = []
+      let wBNBPair = await getPancakePairAddress(input, wBNBAddr, simpleRpcProvider)
+      if (wBNBPair !== null) pairs.push(wBNBPair.toLowerCase())
+      let wBNBPairV1 = await getPancakePairAddressV1(input, wBNBAddr, simpleRpcProvider)
+      if (wBNBPair !== null) pairs.push(wBNBPairV1.toLowerCase())
+      setPairs(pairs)
+    }
+    getPairs()
+    return () => {
+      ab.abort()
+    }
+  }, [tokenAddress])
+
+  const parseData: any = async (events) => {
+    setBusy(true)
+    let newTransactions = stateRef.current
+    return new Promise(async (resolve) => {
+      const price = await getBNBPrice()
+      for (let i = 0; i <= events.length; i++) {
+        if (i === events.length) {
+          setTransactions(newTransactions)
+          setBusy(false)
+          resolve(true)
+        } else {
+          try {
+            const event = events[i]
+            const datas = web3.eth.abi.decodeParameters(
+              [
+                { type: 'uint256', name: 'amount0In' },
+                { type: 'uint256', name: 'amount1In' },
+                { type: 'uint256', name: 'amount0Out' },
+                { type: 'uint256', name: 'amount1Out' },
+              ],
+              event.data,
+            )
+
+            let tokenAmt, BNBAmt, isBuy
+
+            if (input < wBNBAddr) {
+              tokenAmt = Math.abs(
+                parseFloat(ethers.utils.formatUnits(datas.amount0In + '', tokenDecimal)) -
+                  parseFloat(ethers.utils.formatUnits(datas.amount0Out + '', tokenDecimal)),
+              )
+
+              isBuy = datas.amount1In === '0'
+              BNBAmt = Math.abs(
+                parseFloat(ethers.utils.formatUnits(datas.amount1In + '', 18)) -
+                  parseFloat(ethers.utils.formatUnits(datas.amount1Out + '', 18)),
+              )
+            } else {
+              BNBAmt = Math.abs(
+                parseFloat(ethers.utils.formatUnits(datas.amount0In + '', tokenDecimal)) -
+                  parseFloat(ethers.utils.formatUnits(datas.amount0Out + '', tokenDecimal)),
+              )
+              tokenAmt = Math.abs(
+                parseFloat(ethers.utils.formatUnits(datas.amount1In + '', 18)) -
+                  parseFloat(ethers.utils.formatUnits(datas.amount1Out + '', 18)),
+              )
+              isBuy = datas.amount0In === '0'
+            }
+
+            let oneData: any = {}
+            oneData.amount = tokenAmt
+            oneData.price = (BNBAmt / tokenAmt) * price
+            oneData.transactionTime = formatTimeString(
+              `${new Date().getUTCFullYear()}-${
+                new Date().getUTCMonth() + 1
+              }-${new Date().getDate()} ${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getUTCSeconds()}`,
+            )
+            oneData.tx = event.transactionHash
+            oneData.isBuy = isBuy
+            oneData.usdValue = oneData.amount * oneData.price
+            newTransactions.unshift(oneData)
+            if (newTransactions.length > 1000) {
+              newTransactions.pop()
+            }
+            dispatch(priceInput({ price: oneData.price }))
+            dispatch(amountInput({ amount: oneData.usdValue / volumeRate }))
+          } catch (err) {
+            console.log('error', err)
+          }
+        }
+      }
+    })
+  }
+
+  const startRealTimeData = () => {
+    web3.eth.getBlockNumber().then((blockNumber) => {
+      const getTransactions = async (blockNumber) => {
+        let cachedBlockNumber = blockNumber
+        web3.eth
+          .getPastLogs({
+            fromBlock: blockNumber,
+            toBlock: 'latest',
+            topics: ['0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822'],
+          })
+          .then(async (info) => {
+            if (info.length) {
+              cachedBlockNumber = info[info.length - 1].blockNumber
+            }
+            if (pairsRef.current.length === 0) return
+            info = info.filter((oneData) => oneData.blockNumber !== cachedBlockNumber)
+            info = info.filter((oneData) => pairsRef.current.indexOf(oneData.address.toLowerCase()) !== -1)
+            info = [...new Set(info)]
+
+            if (!isBusy) {
+              await parseData(info)
+              blockNumber = cachedBlockNumber
+              return setTimeout(() => getTransactions(blockNumber), 3000)
+            }
+          })
+      }
+
+      getTransactions(blockNumber)
+    })
+  }
+
+  const formatTimeString = (timeString) => {
+    let dateArray = timeString.split(/[- :\/]/)
+    let date = new Date(
+      dateArray[0],
+      dateArray[1] - 1,
+      dateArray[2].slice(0, -1),
+      dateArray[3],
+      dateArray[4],
+      dateArray[5],
+    )
+    return date.toString().split('GMT')[0]
+  }
+
+  useEffect(() => {
+    dispatch(priceInput({ price: UNSET_PRICE }))
+    const fetchDecimals = async () => {
+      tokenDecimal = await contract.methods.decimals().call()
+    }
+    fetchDecimals()
+
+    const ac = new AbortController()
+    let newTransactions = []
+    const fetchData = async () => {
+      try {
+        const provider = simpleRpcProvider // simpleRpcProvider
+        const bnbPrice = await getBNBPrice(provider)
+        setVolumeRate(bnbPrice)
+        // pull historical data
+        const queryResult = await axios.post(BITQUERY_API, { query: getDataQuery() }, config)
+        if (queryResult.data.data && queryResult.data.data.ethereum.dexTrades) {
+          newTransactions = queryResult.data.data.ethereum.dexTrades.map((item, index) => {
+            return {
+              transactionTime: formatTimeString(item.block.timestamp.time),
+              amount: item.baseAmount,
+              price: item.quotePrice * bnbPrice,
+              usdValue: item.baseAmount * item.quotePrice * bnbPrice,
+              isBuy: item.baseCurrency.symbol === item.buyCurrency.symbol,
+              tx: item.transaction.hash,
+            }
+          })
+
+          setTransactions(newTransactions)
+          setLoading(true)
+
+          setTimeout(() => {
+            startRealTimeData()
+          }, 2000)
+        }
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log('err', err.message)
+      }
+    }
+
+    if (input) {
+      fetchData()
+    }
+
+    return () => ac.abort()
+  }, [tokenAddress])
+
+  React.useEffect(() => {
+    const ab = new AbortController()
     if (tokenAddress && tokenAddress !== '') {
       dispatch(typeInput({ input: tokenAddress }))
+    }
+    return () => {
+      ab.abort()
     }
   }, [dispatch, tokenAddress])
 
@@ -175,13 +470,13 @@ export default function Swap({ history }: RouteComponentProps) {
 
   const parsedAmounts = showWrap
     ? {
-      [Field.INPUT]: parsedAmount,
-      [Field.OUTPUT]: parsedAmount,
-    }
+        [Field.INPUT]: parsedAmount,
+        [Field.OUTPUT]: parsedAmount,
+      }
     : {
-      [Field.INPUT]: independentField === Field.INPUT ? parsedAmount : trade?.inputAmount,
-      [Field.OUTPUT]: independentField === Field.OUTPUT ? parsedAmount : trade?.outputAmount,
-    }
+        [Field.INPUT]: independentField === Field.INPUT ? parsedAmount : trade?.inputAmount,
+        [Field.OUTPUT]: independentField === Field.OUTPUT ? parsedAmount : trade?.outputAmount,
+      }
 
   const { onSwitchTokens, onCurrencySelection, onUserInput, onChangeRecipient } = useSwapActionHandlers()
   const isValid = !swapInputError
@@ -230,11 +525,11 @@ export default function Swap({ history }: RouteComponentProps) {
   const { setRouterType } = useSetRouterType()
 
   useEffect(() => {
-    if(tokenAddress === null || tokenAddress === "" || tokenAddress === undefined) {
+    if (tokenAddress === null || tokenAddress === '' || tokenAddress === undefined) {
       dispatch(
         replaceSwapState({
           outputCurrencyId: 'BNB',
-          inputCurrencyId: "0x2e121Ed64EEEB58788dDb204627cCB7C7c59884c",
+          inputCurrencyId: '0x2e121Ed64EEEB58788dDb204627cCB7C7c59884c',
           typedValue: '',
           field: Field.OUTPUT,
           recipient: null,
@@ -251,7 +546,7 @@ export default function Swap({ history }: RouteComponentProps) {
         }),
       )
     }
-  }, [dispatch, tokenAddress]);
+  }, [dispatch, tokenAddress])
 
   useEffect(() => {
     if (swapRouter === SwapRouter.AUTO_SWAP && trade === undefined) {
@@ -296,7 +591,7 @@ export default function Swap({ history }: RouteComponentProps) {
       })
       .catch((error) => {
         let message = error.message
-        if(error.message.includes("INSUFFICIENT_OUTPUT_AMOUNT")) {
+        if (error.message.includes('INSUFFICIENT_OUTPUT_AMOUNT')) {
           message = messages.SLIPPAGE_ISSUE
         }
         setSwapState({
@@ -433,7 +728,7 @@ export default function Swap({ history }: RouteComponentProps) {
   }, [timeNow, countDownDeadline])
 
   useEffect(() => {
-    if(swapFlag){
+    if (swapFlag) {
       handleSwap()
       dispatch(autoSwap({ swapFlag: false }))
     }
@@ -595,8 +890,8 @@ export default function Swap({ history }: RouteComponentProps) {
                         {priceImpactSeverity > 3 && !isExpertMode
                           ? t('Price Impact High')
                           : priceImpactSeverity > 2
-                            ? t('Swap Anyway')
-                            : t('Swap')}
+                          ? t('Swap Anyway')
+                          : t('Swap')}
                       </Button>
                     </RowBetween>
                   ) : (
@@ -623,8 +918,8 @@ export default function Swap({ history }: RouteComponentProps) {
                         (priceImpactSeverity > 3 && !isExpertMode
                           ? `Price Impact Too High`
                           : priceImpactSeverity > 2
-                            ? t('Swap Anyway')
-                            : t('Swap'))}
+                          ? t('Swap Anyway')
+                          : t('Swap'))}
                     </Button>
                   )}
                   {showApproveFlow && (
@@ -660,7 +955,7 @@ export default function Swap({ history }: RouteComponentProps) {
             textAlign: 'center',
           }}
         >
-          {swapTransCard === 'tokenDX' && <TransactionCard tokenAddress={tokenAddress} />}
+          {swapTransCard === 'tokenDX' && <TransactionCard transactionData={transactionData} isLoading={isLoading} />}
           {swapTransCard === 'buyers' && <BuyersCard />}
           {swapTransCard === 'sellers' && <SellersCard />}
         </div>
