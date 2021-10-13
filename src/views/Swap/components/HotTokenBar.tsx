c8ec532e707e4bb25d8601dedcd7238c9dd1292e
loveFeynman <wando0226@gmail.com>
2021-10-13 05:16:50 -0400

    minor fixes and deploy to the server

--- src/views/Swap/components/HotTokenBar.tsx
+++ src/views/Swap/components/HotTokenBar.tsx
@@ -4,11 +4,11 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import Marquee from 'react-fast-marquee'
-import { useDispatch, useSelector } from 'react-redux'
-import moment from 'moment'
+// import { useDispatch, useSelector } from 'react-redux'
+// import moment from 'moment'
 import { useTranslation } from 'contexts/Localization'
 import { HotTokenType } from './types'
-import { AppState } from '../../../state'
+// import { AppState } from '../../../state'
 
 export interface HotTokenBarProps {
   tokens?: HotTokenType[] | null
