425a8469f8b9a224801a500f61f44d63ee5e633a
loveFeynman <wando0226@gmail.com>
2021-11-11 03:01:43 -0500

    updated howtoplay page of lottery

--- src/views/LotterySphx/components/HowToPlay.tsx
+++ src/views/LotterySphx/components/HowToPlay.tsx
@@ -7,32 +7,35 @@ import WebFont from 'webfontloader'
 import LotteryStep1 from 'assets/svg/icon/LotteryStep1.svg'
 import LotteryStep2 from 'assets/svg/icon/LotteryStep2.svg'
 import LotteryStep3 from 'assets/svg/icon/LotteryStep3.svg'
-
-import { BallWithNumber, MatchExampleA, MatchExampleB, PoolAllocationChart } from '../svgs'
+import LotteryWinningCardSapmple from 'assets/svg/icon/LotteryWinningCardSample.svg'
+import LotteryWinningCardWSapmple from 'assets/svg/icon/LotteryWinningCardWSample.svg'
+import LotteryDMatchDark from 'assets/svg/icon/LotteryDMatchDark.svg'
+import LotteryDMatchWhite from 'assets/svg/icon/LotteryDMatchWhite.svg'
+import LotteryLatestIcon from 'assets/images/winning-mark.png'
+import LotteryFundsIcon from 'assets/images/funds.png'
+import Question from 'assets/images/question.png'
+import { ReactComponent as ListMaker } from 'assets/svg/icon/ListMaker.svg'
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items:center;
   background: ${({ theme }) => (theme.isDark ? '#0E0E26' : '#2A2E60')};
   position: relative;
 `
 const Divider = styled.div`
-  background-color: ${({ theme }) => theme.colors.cardBorder};
+  background-color: #21214A;
   height: 1px;
-  margin: 40px 0;
-  width: 100%;
+  width: 80%;
 `
 
 const BulletList = styled.ul`
   list-style-type: none;
-  margin-left: 8px;
   padding: 0;
   li {
     margin: 0;
     padding: 0;
-  }
-  li::before {
-    content: '•';
-    margin-right: 4px;
-    color: ${({ theme }) => theme.colors.textSubtle};
   }
   li::marker {
     font-size: 12px;
@@ -63,6 +66,13 @@ const StyledStepCard = styled(Box)`
   border: 1px solid rgba(255, 255, 255, 0.1);
 `
 
+const GappedFlex = styled(Flex)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background: ${({ theme }) => (theme.isDark ? '#0E0E26' : '#2A2E60')};
+  gap: 52px;
+`
+
 const StyledStepCardTop = styled(Box)`
   display: flex;
   align-self: baseline;
@@ -78,14 +88,7 @@ const StyledStepCardTop = styled(Box)`
   border: ${({ theme }) => (theme.isDark ? '6px solid #0E0E26' : '6px solid #2A2E60')};
 `
 
-const StepCardInner = styled(Box)`
-  width: 100%;
-  height: auto;
-  min-height: 200px;
-  padding: 24px;
-`
-
-type Step = { title: string; subtitle: string; label: string; img: any}
+type Step = { title: string; subtitle: string; label: string; img: any }
 
 const StepCard: React.FC<{ step: Step }> = ({ step }) => {
 
@@ -101,7 +104,7 @@ const StepCard: React.FC<{ step: Step }> = ({ step }) => {
     <StyledStepCard width="100%">
       <Flex flexDirection="column" width="100%">
         <StyledStepCardTop>
-          <img src={step.img} alt=""/>
+          <img src={step.img} alt="" />
         </StyledStepCardTop>
         <Text mt="16px" fontSize="18px" color="#F2C94C" bold textAlign="center">
           {step.label}
@@ -109,155 +112,33 @@ const StepCard: React.FC<{ step: Step }> = ({ step }) => {
         <Text mb="8px" mt="11px" fontSize="20px" bold color="white" textAlign="center">
           {step.title}
         </Text>
-        <Text color="white" px="25%" textAlign="center">{step.subtitle}</Text>
+        <Text color="#777777" px={step.img === LotteryStep2 ? "10%" : "25%"} textAlign="center">{step.subtitle}</Text>
       </Flex>
     </StyledStepCard>
   )
 }
 
-const BallsContainer = styled(Flex)`
-  gap: 6.5px;
-  padding-left: 7px;
-  align-items: center;
-  width: 100%;
-`
-
 const InlineLink = styled(Link)`
   display: inline;
   text-decoration: underline;
-  
+  color: #B928AF;
+  font-size: 14px;
 `
-
-const ExampleBalls = () => {
-  const { isDesktop } = useMatchBreakpoints()
-  const ballSize = isDesktop ? '24px' : '32px'
-  const fontSize = isDesktop ? '14px' : '16px'
-  return (
-    <BallsContainer>
-      <BallWithNumber size={ballSize} fontSize={fontSize} color="yellow" number="9" />
-      <BallWithNumber size={ballSize} fontSize={fontSize} color="green" number="1" />
-      <BallWithNumber size={ballSize} fontSize={fontSize} color="aqua" number="3" />
-      <BallWithNumber size={ballSize} fontSize={fontSize} color="teal" number="6" />
-      <BallWithNumber size={ballSize} fontSize={fontSize} color="lilac" number="6" />
-      <BallWithNumber size={ballSize} fontSize={fontSize} color="pink" number="2" />
-    </BallsContainer>
-  )
-}
-
-const MatchExampleContainer = styled.div`
-  height: 100%;
-  display: grid;
-  grid-template-columns: 1fr 5fr;
-  grid-template-rows: 46px 64px 64px;
-`
-
-const MatchExampleCard = () => {
-  const { isDark } = useTheme()
-  const { isXs } = useMatchBreakpoints()
-  const { t } = useTranslation()
-  const exampleWidth = isXs ? '210px' : '258px'
-  return (
-    <StyledStepCard width={['280px', '330px', '380px']}>
-      <StepCardInner height="220px">
-        <MatchExampleContainer>
-          <Box />
-          <ExampleBalls />
-          <Text lineHeight="72px" textAlign="right" color="secondary" bold mr="20px">
-            {t('A')}
-          </Text>
-          <MatchExampleA width={exampleWidth} height="46px" isDark={isDark} />
-          <Text lineHeight="72px" textAlign="right" color="secondary" bold mr="20px">
-            {t('B')}
-          </Text>
-          <MatchExampleB width={exampleWidth} height="46px" isDark={isDark} />
-        </MatchExampleContainer>
-      </StepCardInner>
-    </StyledStepCard>
-  )
-}
-
-const AllocationGrid = styled.div`
-  display: grid;
-  grid-template-columns: 4fr 1fr;
-  grid-auto-rows: max-content;
-  row-gap: 4px;
-`
-
-const AllocationColorCircle = styled.div<{ color: string }>`
-  border-radius: 50%;
-  width: 20px;
-  height: 20px;
-  margin-right: 8px;
-  background-color: ${({ color }) => color};
-`
-
-const AllocationMatch: React.FC<{ color: string; text: string }> = ({ color, text }) => {
-  return (
-    <Flex alignItems="center">
-      <AllocationColorCircle color={color} />
-      <Text color="white">{text}</Text>
-    </Flex>
-  )
-}
 
 const PoolAllocations = () => {
   const { t } = useTranslation()
+  const theme = useTheme();
   return (
-    <StyledStepCard width={['280px', '330px', '380px']}>
-      <StepCardInner height="auto">
-        <Flex mb="32px" justifyContent="center">
-          <PoolAllocationChart width="100px" height="100px" />
-        </Flex>
-        <Flex justifyContent="space-between">
-          <Text fontSize="12px" color="white" bold textTransform="uppercase">
-            {t('Digits matched')}
-          </Text>
-          <Text fontSize="12px" color="white" bold textAlign="right" textTransform="uppercase">
-            {t('Prize pool allocation')}
-          </Text>
-        </Flex>
-        <AllocationGrid>
-          <AllocationMatch color="#DD00B2" text={t('Matches first %digits%', { digits: 1 })} />
-          <Text textAlign="right" color='white' bold>
-            2%
-          </Text>
-          <AllocationMatch color="#FF7782" text={t('Matches first %digits%', { digits: 2 })} />
-          <Text textAlign="right" color='white' bold>
-            3%
-          </Text>
-          <AllocationMatch color="#998800" text={t('Matches first %digits%', { digits: 3 })} />
-          <Text textAlign="right" color='white' bold>
-            5%
-          </Text>
-          <AllocationMatch color="#77B2FF" text={t('Matches first %digits%', { digits: 4 })} />
-          <Text textAlign="right" color='white' bold>
-            10%
-          </Text>
-          <AllocationMatch color="#E2879F" text={t('Matches first %digits%', { digits: 5 })} />
-          <Text textAlign="right" color='white' bold>
-            20%
-          </Text>
-          <AllocationMatch color="#003EFF" text={t('Matches all 6')} />
-          <Text textAlign="right" color='white' bold>
-            40%
-          </Text>
-          <AllocationMatch color="#CCCCCC" text={t('Burn Pool')} />
-          <Text textAlign="right" color='white' bold>
-            20%
-          </Text>
-        </AllocationGrid>
-      </StepCardInner>
+    <StyledStepCard style={{ border: 'none', background: 'none' }}>
+      <img src={theme.isDark ? LotteryDMatchDark : LotteryDMatchWhite} alt="" />
     </StyledStepCard>
   )
 }
 
-const GappedFlex = styled(Flex)`
-  gap: 24px;
-`
 
 const HowToPlay: React.FC = () => {
   const { t } = useTranslation()
-
+  const theme = useTheme();
   const steps: Step[] = [
     {
       label: t('Step %number%', { number: 1 }),
@@ -297,104 +178,124 @@ const HowToPlay: React.FC = () => {
           ))}
         </StepContainer>
       </Container>
+      <Container style={{ marginTop: "20px" }}>
+        <GappedFlex mx="10%" py="45px" >
+          <Flex flex="2" flexDirection="row" maxWidth="fit-content">
+            <Box minWidth="79px">
+              <img src={LotteryLatestIcon} alt="" />
+            </Box>
+            <Box pl="5px" maxWidth="400px">
+              <Text fontSize="26px" color="white" fontWeight="600">
+                {t('Winning Criteria')}
+              </Text>
+              <Text fontSize="15px" color="#A7A7CC" fontWeight="500">
+                {t('The digits on your ticket must match in the correct order to win.')}
+              </Text>
+              <Text mt="8px" color="#B928AF">
+                {t('Here’s an example lottery draw, with two tickets, A and B.')}
+              </Text>
+              <BulletList >
+                <li >
+                  <Flex mt="12px" ml="-28px" style={{columnGap: '10px'}}>
+                    <Box maxWidth="18px" >
+                      <ListMaker />
+                    </Box>
+                    <Text display="inline" fontSize="14px" color="#A7A7CC" >
+                      {t(
+                        'Ticket A: The first 3 digits and the last 2 digits match, but the 4th digit is wrong, so this ticket only wins a “Match first 3” prize.',
+                      )}
+                    </Text>
+                  </Flex>
+                </li>
+                <li>
+                  <Flex mt="12px" ml="-28px" style={{columnGap: '10px'}}>
+                    <Box maxWidth="18px">
+                      <ListMaker />
+                    </Box>
+                    <Text display="inline"  fontSize="14px" color="#A7A7CC">
+                      {t(
+                        'Ticket B: Even though the last 5 digits match, the first digit is wrong, so this ticket doesn’t win a prize.',
+                      )}
+                    </Text>
+                  </Flex>
+                </li>
+              </BulletList>
+              <Text mt="16px" fontSize="14px" color="#A7A7CC">
+                {t(
+                  'Prize brackets don’t ‘stack’: if you match the first 3 digits in order, you’ll only win prizes from the ‘Match 3’ bracket, and not from ‘Match 1’ and ‘Match 2’.',
+                )}
+              </Text>
+            </Box>
+          </Flex>
+          <Flex flex="1" justifyContent="center">
+            <img src={theme.isDark ? LotteryWinningCardSapmple : LotteryWinningCardWSapmple} alt="" />
+          </Flex>
+        </GappedFlex>
+        <Divider style={{margin: "0px 48px"}} />
+        <GappedFlex mx="10%" py="65px">
+          <Flex flex="1" justifyContent="center">
+            <PoolAllocations />
+          </Flex>
+          <Flex flex="2" flexDirection="column">
+            <Flex maxWidth="fit-content">
+              <Flex maxWidth="61px" maxHeight="70px">
+                <img src={LotteryFundsIcon} width="69px" height="78px" alt="funds" />
+              </Flex>
+              <Box pl="5px" maxWidth="400px">
+                <Heading  mb="13px" scale="lg" color="white">
+                  {t('Prize Funds')}
+                </Heading>
+                <Text color="#A7A7CC" fontSize="15px" lineHeight="142%">
+                  {t('The prizes for each lottery round come from three sources:')}
+                </Text>
+                <Text m="16px 0px 4px" fontSize="15px" lineHeight="142%">
+                  {t('Ticket Purchases')}
+                </Text>
+                <Text display="inline" color="#A7A7CC" lineHeight="142%">
+                  {t('100% of the Sphynx paid by people buying tickets that round goes back into the prize pools.')}
+                </Text>
+                <Text m="16px 0px 4px" fontSize="15px" lineHeight="142%" >
+                  {t('Rollover Prizes')}
+                </Text>
+                <Text display="inline" color="#A7A7CC" lineHeight="142%">
+                  {t(
+                    'After every round, if nobody wins in one of the prize brackets, the unclaimed Sphynx for that bracket rolls over into the next round and are redistributed among the prize pools.',
+                  )}
+                </Text>
+                <Text m="16px 0px 4px" lineHeight="142%" >
+                  {t('Sphynx Injections')}
+                </Text>
+                <Text display="inline" color="#A7A7CC" lineHeight="142%">
+                  {t(
+                    'Sphynx tokens from the contract is added to the lottery each and every day. This Sphynx is of course also included in rollovers! Read more in our guide to ',
+                  )}
+                  <InlineLink href="https://www.sphynxtoken.co/static/media/whitepaper.88cdeecb.pdf" target="_blank" color="">
+                    {t('Sphynx Tokenomics')}
+                  </InlineLink>
+                </Text>
+              </Box>
+            </Flex>
+          </Flex>
+        </GappedFlex>
+      </Container>
 
-      <Divider />
-      <GappedFlex flexDirection={['column', 'column', 'column', 'row']}>
-        <Flex flex="2" flexDirection="column">
-          <Heading mb="24px" scale="lg" color="secondary">
-            {t('Winning Criteria')}
-          </Heading>
-          <Heading mb="24px" scale="md" color="white">
-            {t('The digits on your ticket must match in the correct order to win.')}
-          </Heading>
-          <Text mb="8px" ml="8px" color="white">
-            {t('Here’s an example lottery draw, with two tickets, A and B.')}
-          </Text>
-          <BulletList >
-            <li style={{ marginLeft: '8px' }}>
-              <Text display="inline" color="white" style={{ textIndent: '-1em' }}>
-                {t(
-                  'Ticket A: The first 3 digits and the last 2 digits match, but the 4th digit is wrong, so this ticket only wins a “Match first 3” prize.',
-                )}
-              </Text>
-            </li>
-            <li style={{ marginLeft: '8px' }}>
-              <Text display="inline" color="white">
-                {t(
-                  'Ticket B: Even though the last 5 digits match, the first digit is wrong, so this ticket doesn’t win a prize.',
-                )}
-              </Text>
-            </li>
-          </BulletList>
-          <Text ml="8px" mt="16px" color="white">
-            {t(
-              'Prize brackets don’t ‘stack’: if you match the first 3 digits in order, you’ll only win prizes from the ‘Match 3’ bracket, and not from ‘Match 1’ and ‘Match 2’.',
-            )}
-          </Text>
-        </Flex>
-        <Flex flex="1" justifyContent="center">
-          <MatchExampleCard />
-        </Flex>
-      </GappedFlex>
-      <Divider />
-      <GappedFlex flexDirection={['column', 'column', 'column', 'row']}>
-        <Flex flex="2" flexDirection="column">
-          <Heading mb="24px" scale="lg" color="secondary">
-            {t('Prize Funds')}
-          </Heading>
-          <Text color="white">{t('The prizes for each lottery round come from three sources:')}</Text>
-          <Heading my="16px" scale="md">
-            {t('Ticket Purchases')}
-          </Heading>
-          <BulletList>
-            <li>
-              <Text display="inline" color="white">
-                {t('100% of the Sphynx paid by people buying tickets that round goes back into the prize pools.')}
-              </Text>
-            </li>
-          </BulletList>
-          <Heading my="16px" scale="md">
-            {t('Rollover Prizes')}
-          </Heading>
-          <BulletList>
-            <li>
-              <Text display="inline" color="white">
-                {t(
-                  'After every round, if nobody wins in one of the prize brackets, the unclaimed Sphynx for that bracket rolls over into the next round and are redistributed among the prize pools.',
-                )}
-              </Text>
-            </li>
-          </BulletList>
-          <Heading my="16px" scale="md">
-            {t('Sphynx Injections')}
-          </Heading>
-          <BulletList>
-            <li>
-              <Text display="inline" color="white">
-                {t(
-                  'Sphynx tokens from the contract is added to the lottery each and every day. This Sphynx is of course also included in rollovers! Read more in our guide to ',
-                )}
-                <InlineLink href="https://www.sphynxtoken.co/static/media/whitepaper.88cdeecb.pdf" target="_blank">
-                  {t('Sphynx Tokenomics')}
-                </InlineLink>
-              </Text>
-            </li>
-          </BulletList>
-        </Flex>
-        <Flex flex="1" justifyContent="center">
-          <PoolAllocations />
-        </Flex>
-      </GappedFlex>
-      <Divider />
-      <Flex justifyContent="center" alignItems="center" flexDirection={['column', 'column', 'row']} mb="32px">
-        <img width={240} src="/logo.png" alt="Sphynx" />
-        <Flex maxWidth="300px" flexDirection="column">
-          <Heading mb="16px" scale="md">
+      <Flex 
+        justifyContent="center" 
+        alignItems="center" 
+        flexDirection="column" 
+        mt="40px" 
+        mb="66px" 
+        style={{textAlign: 'center'}}
+      >
+        {/* <img src={LotteryDMatchWhite} alt="question" /> */}
+        <img src={Question} alt="question" />
+        <Flex flexDirection="column" >
+          <Text mt="15px" fontSize="20px">
             {t('Still got questions?')}
-          </Heading>
-          <Text>
+          </Text>
+          <Text fontSize="12px" mt="11px" color="#A7A7CC">
             {t('Check our in-depth guide on')}{' '}
-            <InlineLink href="/Lottery.pdf" target="_blank">
+            <InlineLink href="/Lottery.pdf" target="_blank" fontSize="12px">
               {t('how to play the Sphynx lottery!')}
             </InlineLink>
           </Text>
@@ -405,3 +306,4 @@ const HowToPlay: React.FC = () => {
 }
 
 export default HowToPlay
+
