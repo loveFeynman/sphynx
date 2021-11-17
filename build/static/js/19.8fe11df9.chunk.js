8cae84ab8850ddf8cb8f42032101e37a16016dc1
loveFeynman <wando0226@gmail.com>
2021-11-17 00:53:50 -0500

    minor fixes and deploy to the server

--- build/static/js/19.8fe11df9.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[19],{1645:function(n,p){}}]);
---
+++ build/static/js/19.b45844cf.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[19],{1646:function(n,p){}}]);
