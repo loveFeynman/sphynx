6b5c29ab60763d3d5f64a7ac3d7748ded66062f0
loveFeynman <wando0226@gmail.com>
2021-11-18 09:19:57 -0500

    minor fixes and deploy to the server

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -3,12 +3,11 @@ import Web3 from 'web3'
 import getRpcUrl from 'utils/getRpcUrl'
 
 const RPC_URL = getRpcUrl()
-const RPC_URL_ARCHIEVE = `${getRpcUrl()}/archive`
 
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL)
 export const web3Provider = new Web3.providers.HttpProvider(RPC_URL);
 
-export const web3ArchiveProvider = new Web3.providers.HttpProvider('https://bsc.getblock.io/mainnet/?api_key=249a480d-6030-4050-9bcc-cf35941da929');
+export const web3ArchiveProvider = new Web3.providers.HttpProvider(RPC_URL);
 
 export const WEBSOCKET_URL = process.env.REACT_APP_WSNODE_1
 // export const simpleWebsocketProvider = new ethers.providers.WebSocketProvider(WEBSOCKET_URL)
