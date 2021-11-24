491f9a3a685f911a2ef564bd47700f52d6934a1b
loveFeynman <wando0226@gmail.com>
2021-11-24 16:59:04 -0500

    minor fixes

--- src/components/DisclaimerModal/DisclaimerModal.tsx
+++ src/components/DisclaimerModal/DisclaimerModal.tsx
@@ -15,6 +15,7 @@ import styled from 'styled-components'
 import { TERMS_LIST } from './config'
 
 const StyledModalContainer = styled(ModalContainer)`
+  font-size: 20px;
   ${({ theme }) => theme.mediaQueries.sm} {
     width: 420px;
   }
@@ -110,7 +111,7 @@ export default function DisclaimerModal({ onDismiss = () => null, modalState }: 
                 checked={(arrayCheckedState.indexOf(index) > -1) as boolean}
                 onClick={() => handleChangeCheckbox(index)}
               />
-              <Text fontSize="12px">{item.text}</Text>
+              <Text fontSize="16px">{item.text}</Text>
             </TermRow>
           ))}
         </CheckGroup>
