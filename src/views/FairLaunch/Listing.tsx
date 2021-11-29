9e2dd0299ce09dc8b5b4a000735e8167eb8fc56c
loveFeynman <wando0226@gmail.com>
2021-11-29 02:25:16 -0500

    feat: [fair-launch-listing] implement fair-launch/listing page ui

--- src/views/FairLaunch/Listing.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FairLaunchListing: React.FC = () => {
-  return <div>fair launch listing page</div>
-}
-
-export default FairLaunchListing
--- src/views/FairLaunch/Listings.tsx
+++ src/views/FairLaunch/Listings.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 import Pagination from '@material-ui/lab/Pagination'
 import { useTranslation } from 'contexts/Localization'
 import styled from 'styled-components'
 import axios from 'axios'
 import * as ethers from 'ethers'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { simpleRpcProvider } from 'utils/providers'
 import { getPresaleContract } from 'utils/contractHelpers'
 import ListIcon from 'assets/svg/icon/ListIcon.svg'
 import { useMenuToggle } from 'state/application/hooks'
 import Spinner from 'components/Loader/Spinner'
-import { SEARCH_OPTION, LAUNCHPAD_NUM_PER_PAGE } from 'config/constants/launchpad'
 import TokenCard from './components/TokenCard'
 import SearchPannel from './components/SearchPanel'
 
@@ -23,6 +23,8 @@ import {
   PaginationWrapper,
 } from './ListingsStyles'
 
+const presaleContract = getPresaleContract(simpleRpcProvider)
+
 const LoadingWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -30,36 +32,17 @@ const LoadingWrapper = styled.div`
   width: 100%;
 `
 
-const Presale: React.FC = () => {
-  const { chainId, library } = useActiveWeb3React()
-  const presaleContract = useMemo(() => getPresaleContract(library), [library])
+const FairLaunchListing: React.FC = () => {
+  const { chainId } = useActiveWeb3React()
   const { t } = useTranslation()
   const { menuToggled } = useMenuToggle()
   const [tokenList, setTokenList] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
-  const [searchOption, setSearchOption] = useState(SEARCH_OPTION.ALL)
-  const [searchKey, setSearchKey] = useState('')
-  const [pageIndex, setPageIndex] = useState(0)
-  const [pageCount, setPageCount] = useState(1)
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = {
-        chain_id: chainId,
-        token_level: searchOption,
-        key: searchKey,
-        page_index: pageIndex,
-        num_per_page: LAUNCHPAD_NUM_PER_PAGE,
-      }
-
-      setIsLoading(true)
-      setTokenList([])
-      axios.post(`${process.env.REACT_APP_BACKEND_API_URL2}/getAllPresaleInfo`, { data }).then(async (response) => {
+      axios.get(`${process.env.REACT_APP_BACKEND_API_URL2}/getAllPresaleInfo/${chainId}`).then(async (response) => {
         if (response.data) {
-          let pages = 1
-          if(response.data.length > 0)
-           pages = Math.ceil(parseInt(response.data[0].count) / LAUNCHPAD_NUM_PER_PAGE)
-          setPageCount(pages)
           try {
             const list = await Promise.all(
               response.data.map(async (cell) => {
@@ -110,18 +93,12 @@ const Presale: React.FC = () => {
             console.log('error', error)
             setIsLoading(false)
           }
-        } else {
-          setIsLoading(false)
         }
       })
     }
 
-    if (chainId && searchOption !== undefined) fetchData()
-  }, [chainId, searchOption, searchKey, pageIndex, presaleContract])
-
-  const handlePageIndex = (e, page) => {
-    setPageIndex(page - 1)
-  }
+    if (chainId) fetchData()
+  }, [chainId])
 
   return (
     <Wrapper>
@@ -129,11 +106,11 @@ const Presale: React.FC = () => {
         <TitleWrapper>
           <img src={ListIcon} alt="listIcon" />
           <Title>
-            <LogoTitle>LaunchPad Listings</LogoTitle>
+            <LogoTitle>Fair LaunchPad Listing</LogoTitle>
           </Title>
         </TitleWrapper>
       </HeaderWrapper>
-      <SearchPannel setSearchOption={setSearchOption} setSearchKey={setSearchKey} setPageIndex={setPageIndex}/>
+      <SearchPannel />
       {isLoading && (
         <LoadingWrapper>
           <Spinner />
@@ -154,8 +131,6 @@ const Presale: React.FC = () => {
               hardCap={item.hardCap}
               minContribution={item.minContribution}
               maxContribution={item.maxContribution}
-              startTime={item.startTime}
-              endTime={item.endTime}
               tokenState={item.tokenState}
             >
               <img src={item.tokenLogo} alt="token icon" />
@@ -163,14 +138,10 @@ const Presale: React.FC = () => {
           ))}
       </TokenListContainder>
       <PaginationWrapper>
-        <Pagination 
-        count={pageCount} 
-        siblingCount={0} 
-        onChange={handlePageIndex}
-        />
+        <Pagination count={5} siblingCount={0} />
       </PaginationWrapper>
     </Wrapper>
   )
 }
 
-export default Presale
+export default FairLaunchListing
